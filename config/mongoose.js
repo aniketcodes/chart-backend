01116df9b46e4890d6146893366b507b42417fe3
aniketcodes <aniketsinha1304@gmail.com>
2021-10-05 11:32:36 +0530

    moved db to online

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require( "mongoose" )
 
-mongoose.connect( `mongodb://localhost/chart` )
-
+//mongoose.connect( `mongodb://localhost/chart` )
+mongoose.connect(`mongodb+srv://abhishek:abhishek@cluster0.zekva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
 const db = mongoose.connection
 db.on( 'error', console.error.bind( console, "Error in connecting to db" ) )
 db.once( 'open', () => {
