0437c72573dfc27789f386eadac9adb7611ea025
Aniket S <52822404+aniketcodes@users.noreply.github.com>
2021-10-11 06:11:44 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require( "mongoose" )
 
-//mongoose.connect( `mongodb://localhost/chart` )
-mongoose.connect(`mongodb+srv://abhishek:abhishek@cluster0.zekva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+mongoose.connect( `mongodb://localhost/chart` )
 const db = mongoose.connection
 db.on( 'error', console.error.bind( console, "Error in connecting to db" ) )
 db.once( 'open', () => {
